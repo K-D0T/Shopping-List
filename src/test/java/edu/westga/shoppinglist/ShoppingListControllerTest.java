727e152e5e8983a9dcf44740096bfbc03d863992
unknown <kaiden.thrailkill@gmail.com>
2024-04-11 10:13:51 -0400

    Alter maven.yml file to pass check tests.

--- src/test/java/edu/westga/shoppinglist/ShoppingListControllerTest.java
+++ src/test/java/edu/westga/shoppinglist/ShoppingListControllerTest.java
@@ -5,44 +5,32 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-
 import org.junit.jupiter.api.Test;
 import javafx.scene.control.TextField;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.testfx.util.WaitForAsyncUtils;
-import javafx.application.Platform;
-
-
-
 
 public class ShoppingListControllerTest extends ApplicationTest {
     private ShoppingListController controller;
 
-
     @Override
     public void start(Stage stage) throws Exception {
-        Platform.runLater(() -> {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
-                Parent root = loader.load();
-                this.controller = loader.getController();
-                stage.setScene(new Scene(root));
-                stage.show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        WaitForAsyncUtils.waitForFxEvents();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        Parent root = loader.load();
+        this.controller = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
-    
+
     @Test
     public void testGetItemName() {
         WaitForAsyncUtils.waitForFxEvents();
         TextField itemNameField = this.controller.getItemName();
         assertNotNull(itemNameField, "Item name field should not be null");
     }
+
 
     @Test
     public void testAddItemToListWithEmptyName() {
